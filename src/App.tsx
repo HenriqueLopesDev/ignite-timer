245b5ee4c8e2a8e8bec9c6c170bd2eb91ffddc2f
Henrique Lopes <henriquebonfanti2018@gmail.com>
2022-11-06 15:12:08 -0300

    Create the cycles context.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
@@ -9,7 +10,9 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
